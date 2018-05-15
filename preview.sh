6a74d27d15191adc4e7a6e7b35e6c75d711b8021
Dan Haywood <dan@haywood-associates.co.uk>
2018-05-15 23:42:54 +0100

    updates presentation for ncrafts

--- preview.sh
+++ preview.sh
@@ -1,1 +1,1 @@
-./gradlew --offline asciidoctor && pushd build/asciidoc/revealjs && python -m SimpleHTTPServer 8000
+./gradlew --offline asciidoctor &&  python -m webbrowser -t http://localhost:4000  && pushd build/asciidoc/revealjs && python -m http.server 4000 && popd
